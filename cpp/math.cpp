c789066627ff05e8a1cb7d2829165f70fc46b65d
Nakkouch Tarek <98561646+nakkouchtarek@users.noreply.github.com>
2023-05-04 19:44:13 +0100

    Update math.cpp

--- cpp/math.cpp
+++ cpp/math.cpp
@@ -1,14 +1,14 @@
 int expo(int num,int val)
 {
-    int num2 = num; // num2 = 2
-    int val2 = val; // val2 = 2 
+    int num2 = num;
+    int val2 = val; 
 
     if(val2==0)
     {
         return 1;
     }
 
-    while(val2!=1) // while val2 >= 0
+    while(val2!=1)
     {
         num2 *= num; // num2 = 2 * 2
         val2--; // val2 = val2 - 1 
